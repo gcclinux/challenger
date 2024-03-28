6dc2772f80dc59e2b5d4c2c2011e1930c848722d
Ricardo Wagemaker <java@wagemaker.co.uk>
2024-03-28 23:02:39 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Cool beans!<br></br>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a

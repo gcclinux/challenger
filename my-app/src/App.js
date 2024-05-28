c4dff57ad3da05631d2ec43a7ed898f686814e27
Ricardo Wagemaker <java@wagemaker.co.uk>
2024-05-28 23:35:56 +0100

    added input and check against array

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,27 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+
+  const wordsArray = ['Go', 'Golang', 'React', 'JavaScript', 'Python' , 'Java' , 'C++' , 'C#' , 'Ruby' , 'Rust' , 'Swift' , 'Kotlin' , 'TypeScript' , 'PHP' , 'HTML' , 'CSS' , 'SQL' , 'NoSQL'];
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <label>Enter valid programming languages</label>
+        <br />
+        <input
+          type="text"
+          style={{ width: '200px' }}
+          value={inputValue}
+          onChange={e => setInputValue(e.target.value)}
+        />
+        {wordsArray.includes(inputValue) ? <p>{inputValue} OK</p> : <p>{inputValue} Incorrect</p>}
+
       </header>
     </div>
   );
